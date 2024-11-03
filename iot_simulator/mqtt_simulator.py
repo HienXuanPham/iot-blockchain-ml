2418d3061035ab39f135e175c213783f49790f6d
HienXuanPham <phamxuanhien02@gmail.com>
2024-11-03 18:59:31 -0500

    generate random values for IoT simulators

--- iot_simulator/mqtt_simulator.py
+++ iot_simulator/mqtt_simulator.py
@@ -4,12 +4,12 @@ import time
 
 def simulate_normal_data(client, topic):
   for i in range(30):
-    client.publish(topic, f"{i}: normal data")
+    client.publish(topic, random.randint(10, 50))
     time.sleep(random.uniform(0.7, 2.1))
   
 def simulate_anomaly_data(client, topic):
   for i in range(70):
-    client.publish(topic, f"{i}: anomaly data")
+    client.publish(topic, random.randint(100, 300))
     time.sleep(random.uniform(0.01, 0.09))
 
 def main():
