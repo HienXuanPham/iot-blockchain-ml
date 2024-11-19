6139a0ca4548bc098237a99a486bef1c0c73a366
HienXuanPham <phamxuanhien02@gmail.com>
2024-11-19 00:58:43 -0500

    split data and probably downsize the dataset

--- data_preparation/data_preparation.py
+++ data_preparation/data_preparation.py
@@ -2,20 +2,28 @@ import pandas as pd
 import numpy as np
 import os
 from tqdm import tqdm
+from sklearn.model_selection import train_test_split
 
-DATASET_PATH = '../CICIoT2023/'
+# DATASET_PATH = '../CICIoT2023/'
 
-csv_files = [file for file in os.listdir(DATASET_PATH) if file.endswith('.csv')]
+# csv_files = [file for file in os.listdir(DATASET_PATH) if file.endswith('.csv')]
 
-csv_files.sort()
+# csv_files.sort()
 
-dataset_df = pd.DataFrame()
-print("CSV files to be processed:", csv_files)
+# dataset_df = pd.DataFrame()
+# print("CSV files to be processed:", csv_files)
 
-for file in tqdm(csv_files):
-    file_path = os.path.join(DATASET_PATH, file)
-    df = pd.read_csv(file_path)
-    dataset_df = pd.concat([dataset_df, df], ignore_index=True)
+# for file in tqdm(csv_files):
+#     file_path = os.path.join(DATASET_PATH, file)
+#     df = pd.read_csv(file_path)
+#     dataset_df = pd.concat([dataset_df, df], ignore_index=True)
 
-dataset_df.to_csv('dataset.csv', index=False)
-print("Dataset created and saved as dataset.csv")
+# dataset_df.to_csv('dataset.csv', index=False)
+# print("Dataset created and saved as dataset.csv")
+
+dataset = pd.read_csv('dataset.csv')
+small_dataset = dataset.sample(frac=0.1, random_state=42)
+train_data, test_data = train_test_split(small_dataset, test_size=0.2, random_state=42)
+print(len(train_data), len(test_data)) 
+# Output: 37349263 rows for train_data and 9337316 rows for test_data when using the full dataset
+# Output: 3734926 and 933732 rows for train_data and test_data when using the 10% dataset
